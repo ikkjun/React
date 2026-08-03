a31b13d7ae393941d61c36d20ac0f3fbedc4073e
ChoIkJun <89628722+ikkjun@users.noreply.github.com>
2026-08-03 10:28:46 +0900

    state study

--- 3장 컴포넌트/component-test/src/App.js
+++ 3장 컴포넌트/component-test/src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import MyComponent from "./MyComponent";
-import MyComponentClass from "./MyComponentClass";
+// import MyComponent from "./MyComponent";
+// import MyComponentClass from "./MyComponentClass";
+import Counter from "./Counter";
 
 const App = () => {
-  return (
-    <>
-      <MyComponent name="React" favoriteNumber={1}>
-        리액트
-      </MyComponent>
-      <MyComponentClass name="ReactClass" favoriteNumber={1}>
-        리액트 클래스
-      </MyComponentClass>
-    </>
-  );
+  return <Counter />;
+
+  // (
+  //   <>
+  //     <MyComponent name="React" favoriteNumber={1}>
+  //       리액트
+  //     </MyComponent>
+  //     <MyComponentClass name="ReactClass" favoriteNumber={1}>
+  //       리액트 클래스
+  //     </MyComponentClass>
+  //   </>
+  // );
 };
 
 export default App;
---
+++ 3장 컴포넌트/component-test/src/Counter.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    // state의 초깃값 설정하기
+    this.state = {
+      number: 0,
+    };
+  }
+  render() {
+    const { number } = this.state; // state를 조회할 때는 this.state로 조회한다.
+    return (
+      <div>
+        <h1>{number}</h1>
+        <button
+          onClick={() => {
+            this.setState({ number: number + 1 });
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
