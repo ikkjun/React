0084370e9211fed9119b16fb7548308f90ed1f22
ChoIkJun <89628722+ikkjun@users.noreply.github.com>
2026-08-04 12:40:24 +0900

    3장 컴포넌트 정리

--- 3장 컴포넌트/component-test/src/App.js
+++ 3장 컴포넌트/component-test/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 // import MyComponent from "./MyComponent";
 // import MyComponentClass from "./MyComponentClass";
-import Counter from "./Counter";
+// import Counter from "./Counter";
+import Say from "./Say";
 
 const App = () => {
-  return <Counter />;
+  // return <Counter />;
 
   // (
   //   <>
@@ -16,6 +17,7 @@ const App = () => {
   //     </MyComponentClass>
   //   </>
   // );
+  return <Say />;
 };
 
 export default App;
